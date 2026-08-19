1bb6bc2ad934d9ea1882a5d8a2f2606af5293773
David Grigoryan <davidgrigoryaaan@proton.me>
2026-08-19 18:42:02 +0300

    feat(nvim): add completion and editing plugins

---
+++ nvim/.config/nvim/lua/david/plugins/blink-cmp.lua
@@ -0,0 +1,72 @@
+return {
+    {
+        "saghen/blink.cmp",
+        version = "v1.*",
+        dependencies = {
+            "L3MON4D3/LuaSnip",
+            "rafamadriz/friendly-snippets",
+        },
+        config = function()
+            require("blink.cmp").setup({
+                fuzzy = {
+                    implementation = "prefer_rust",
+                },
+                keymap = {
+                      preset = "none",
+
+                      -- принять completion
+                      ["<CR>"] = { "accept", "fallback" },
+
+                      -- следующий / предыдущий вариант
+                      ["<Tab>"] = { "select_next", "snippet_forward", "fallback" },
+                      ["<S-Tab>"] = { "select_prev", "snippet_backward", "fallback" },
+
+                      -- закрыть completion
+                      ["<C-e>"] = { "cancel", "fallback" },
+                },
+                completion = {
+                    menu = {
+                        auto_show = true,
+                    },
+                    documentation = {
+                        auto_show = true,
+                    },
+                    ghost_text = {
+                        enabled = false,
+                        show_with_menu = false,
+                    },
+                    accept = {
+                        auto_brackets = {
+                            enabled = true,
+                        },
+                    },
+                },
+                cmdline = {
+                    enabled = true,
+                    keymap = { preset = "cmdline" },
+                    completion = {
+                        menu = { auto_show = true },
+                    },
+                },
+                sources = {
+                    default = { "lsp", "path", "buffer", "snippets" },
+                    providers = {
+                        lsp = {
+                            opts = {
+                                tailwind_color_icon = "󱓻"
+                            }
+                        }
+                    }
+                },
+                appearance = {
+                    use_nvim_cmp_as_default = false,
+                    nerd_font_variant = "mono",
+                },
+                snippets = {
+                    preset = "luasnip"
+                },
+            })
+            require("luasnip.loaders.from_vscode").lazy_load()
+        end,
+    },
+}
